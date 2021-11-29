49ce90a7c6c8f79001fbae89042a4981a85c0d48
NJdongya <63613353+NJdongya@users.noreply.github.com>
2021-11-29 12:12:32 -0500

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,13 +1,13 @@
 const spaceData = require('./space');
 const utilData = require('./util');
-const reviewData = require('./review');
-const commentData = require('./comment');
-const userData = require('./user');
+// const reviewData = require('./review');
+// const commentData = require('./comment');
+// const userData = require('./user');
 
 module.exports = {
     space: spaceData,
     util: utilData,
-    review: reviewData,
-    comment: commentData,
-    user: userData
+    // review: reviewData,
+    // comment: commentData,
+    // user: userData
 };

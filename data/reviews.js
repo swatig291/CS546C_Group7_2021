2ab1d7246f36b14f4714e5699107630e5416a696
NJdongya <63613353+NJdongya@users.noreply.github.com>
2021-11-30 10:22:55 -0500

    Update reviews.js

--- data/reviews.js
+++ data/reviews.js
@@ -1,97 +1,81 @@
 const mongoCollections = require('../config/mongoCollections');
+const { ObjectId } = require('mongodb');
 const reviews = mongoCollections.reviews;
-const verify = require('./util');
-const spaces = require('./space');
-const comments = require('./comments');
-let { ObjectId } = require('mongodb');
+// const spacesCollection = require("./spaces.js");
+
+async function getReviewById(id) {
+    if (!id || typeof id !== "string")
+        throw 'You must provide an id to search for';
+    let reviewCollection = await reviews();
+    let objId = ObjectId.createFromHexString(id);
+    let reviewGoal = await reviewCollection.findOne({ _id: objId });
+    if (reviewGoal === null)
+        throw 'No review with that id';
+    return reviewGoal;
+}
+
+async function getAllReviews() {
+    let reviewCollection = await reviews();
+    let allreviews = await reviewCollection.find({}).toArray();
+    return allreviews;
+}
+
+async function addReview(spaceId, userId, content,rating) {
+
+    if (!spaceId || typeof spaceId !== "string")
+        throw 'you should input a string as the spaceId';
+    if (!userId || typeof userId !== "string")
+        throw 'you should input a string as the userId';
+    if (!content || typeof content !== "string")
+        throw 'you should input a string as the content';
+
+    let reviewCollection = await reviews();
+    let newreview = {
+        spaceId: spaceId,
+        userId: userId,
+        content: content,
+        rating: rating,
+        datetime: new Date().toLocaleDateString()
+    }
+    let insertInfo = await reviewCollection.insertOne(newreview);
+    if (insertInfo === null)
+        throw 'Something wrong when adding the review';
+    let newreviewId = insertInfo.insertedId;
+    let reviewCreated = await getReviewById(newreviewId.toHexString());
+
+    // await spacesCollection.addreviewIdTospace(spaceId, newreviewId.toHexString());
+
+    return reviewCreated;
+}
+
+ async function removeReview(id) {
+    if (!verify.validString(id)) throw 'review id must be a valid string.';
+
+    let objId = ObjectId(id.trim());
+
+    let existingData = await this.getReviewById(id);
+
+    if(existingData === null) throw 'review does not exist for the given Id'
+
+           
+    //delete reviews related to respective Id
+
+    //check delete info.(track)
+
+    let reviewCollection = await reviews();
+    let deletionInfo = await reviewCollection.deleteOne({ _id: objId });
+    if (deletionInfo.deletedCount === 0) {
+        throw `Could not delete the review with id of ${id}`;
+    }
+
+    return true;
+}
+
 
 module.exports = {
-    async getReviewById(reviewId) {
-        if (!verify.validString(reviewId)) throw 'Review id is not a valid string.';
-
-        let parsedId = ObjectId(reviewId);
-
-        const reviewCollection = await reviews();
-        let review = await reviewCollection.findOne({ _id: parsedId });
-        if (review === null) throw 'No review with that id.';
-        review._id = review._id.toString();
-
-        return review;
-    },
-
-    //this one neeed to be updated
-    async createReview(reviewerId, spaceId, reviewText) {
-        if (!verify.validString(reviewerId)) throw 'Reviewer id is not a valid string.';
-        if (!verify.validString(spaceId)) throw 'space id is not a valid string.';
-        if (!verify.validString(reviewText)) throw 'Review text is not a valid string.';
-
-
-        let newReview = {
-            reviewerId: reviewerId,
-            spaceId: spaceId,
-            reviewText: reviewText,
-            rating: [],
-
-        };
-
-    
-        const reviewCollection = await reviews();
-        const insertInfo = await reviewCollection.insertOne(newReview);
-        if (insertInfo.insertedCount === 0) throw 'Could not add review.';
-
-        const newId = insertInfo.insertedId;
-
-        const finReview = await this.getReviewById(newId.toString());
-        return finReview;
-    },
-
-   
-   
-    async deleteReview(reviewId) {
-        if (!verify.validString(reviewId)) throw 'Review id is not a valid string.';
-
-        /* Remove all comments on this review */
-        let commentList = comments.getAllCommentsOfReview(reviewId);
-        if (commentList.length > 0) {
-            for (let commentId of commentList) {
-                await comments.deleteComment(commentId);
-            }
-        }
-
-        let review = await this.getReviewById(reviewId);
-
-        spaces.updateMetrics(review.spaceId, review.metrics, false);
-
-        /* delete review from DB */
-        const reviewCollection = await reviews();
-        const deletionInfo = await reviewCollection.deleteOne({ _id: ObjectId(reviewId) });
-        if (deletionInfo.deletedCount === 0) throw `Could not delete review with id of ${reviewId}.`;
-
-        return review;
-    },
-
-    async getAllReviewsOfUser(reviewerId) {
-        if (!verify.validString(reviewerId)) throw 'Reviewer id is not a valid string.';
-
-        const reviewCollection = await reviews();
-        const reviewList = await reviewCollection.find({ 'reviewerId': { $eq: reviewerId } }).toArray();
-        for (let x of reviewList) {
-            x._id = x._id.toString();
-        }
-
-        return reviewList;
-    },
-
-    async getAllReviewsOfspace(spaceId) {
-        if (!verify.validString(spaceId)) throw 'space id is not a valid string.';
-
-        const reviewCollection = await reviews();
-        const reviewList = await reviewCollection.find({ 'spaceId': { $eq: spaceId } }).toArray();
-        for (let x of reviewList) {
-            x._id = x._id.toString();
-        }
-
-        return reviewList;
-    }
+    getReviewById,
+    addReview,
+    removeReview,
+    getAllReviews
 }
 
